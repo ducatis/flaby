701089e35439e2ebb18892d17648d715be8ba707
Takhir <taxup91@gmail.com>
2021-04-12 12:29:43 +0600

    six commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -61,7 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const galleryContainer = document.querySelector('.gallery-container');
     const galleryControlsContainer = document.querySelector('.gallery-controls');
-    const galleryControls = ['previous', 'add', 'next'];
+    const galleryControls = ['previous', 'next'];
     const galleryItems = document.querySelectorAll('.gallery-item');
 
     class Carousel {
